745a503f73d7d378269b937e635d6f32093b1197
Tal Agmon <tal.agmon@gmail.com>
2024-09-30 19:30:51 +0300

    change app text disply on

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> I was changed! 
+          Edit <code>src/App.js</code> I was changed on feature branch! 
         </p>
         <a
           className="App-link"
